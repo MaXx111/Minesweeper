a4830e85f3d58f2e30125cd51ebbec93fba3fdf2
«MaXx111» <maxx11167@gmail.com>
2023-05-10 18:49:01 +0300

    feat: create mines and click on cells

--- Minesweeper/src/js/Minesweeper.js
+++ Minesweeper/src/js/Minesweeper.js
@@ -3,10 +3,13 @@ import HTMLCode from './htmlCode.js'
 export default class Minesweeper {
     constructor(body) {
         this.body = body;
+        this.table = false;
 
         this.htmlCode = new HTMLCode();
 
         this.printItems = this.printItems.bind(this);
+        this.onCellClick = this.onCellClick.bind(this);
+        this.addMines = this.addMines.bind(this);
     }
 
     init() {
@@ -17,19 +20,54 @@ export default class Minesweeper {
     printItems() {
         this.body.appendChild(this.htmlCode.conteiterHTML())
 
-        let table = document.getElementById('table');
+        this.table = document.getElementById('table');
 
-        this.printCells(table)
+        this.table.addEventListener('click', this.onCellClick)
+
+        this.printCells()
     }
 
-    printCells(table) {
+    printCells() {
         for (var i=0; i<10; i++) {
-            let row = table.insertRow(i);
+            let row = this.table.insertRow(i);
 
             for (var j=0; j<10; j++) {
                 let cell = row.insertCell(j);
+                cell.className = 'cell';
+                let mine = document.createAttribute("data-mine");       
+                mine.value = "false";             
+                cell.setAttributeNode(mine);
             }
         }
+
+        this.addMines();
+    }
+
+    addMines() {
+        for (var i=0; i<20; i++) {
+            let row = Math.floor(Math.random() * 10);
+            let col = Math.floor(Math.random() * 10);
+            let cell = this.table.rows[row].cells[col];
+            cell.setAttribute("data-mine","true");
+            cell.innerHTML="X";
+          }
+    }
+
+    onCellClick(e) {
+        e.preventDefault();
+        if(e.target.className !== 'cell') return;
+
+        let cellTarget = e.target;
+
+        // if(cellTarget.)
+
+        if (cellTarget.dataset.mine == 'true') {
+            alert(`Game over`);
+            return
+        }
+
+        console.log(cellTarget)
+        cellTarget.classList.add('active');
     }
 
 }
