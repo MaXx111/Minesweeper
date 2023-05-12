d364b79b6f74d125c5cd448d7950e5f6ff081d51
«MaXx111» <maxx11167@gmail.com>
2023-05-12 15:29:19 +0300

    feat: create html code for statistic

--- Minesweeper/src/js/htmlCode.js
+++ Minesweeper/src/js/htmlCode.js
@@ -1,6 +1,7 @@
 export default class HTMLCode {
     constructor () {
 
+        this.localStorage = false;
     }
 
     conteiterHTML() {
@@ -20,8 +21,69 @@ export default class HTMLCode {
 
         wrapper.appendChild(h1);
         wrapper.appendChild(table);
+        wrapper.appendChild(this.statisticHTML());
 
         conteiner.appendChild(wrapper);
         return conteiner;
+    }
+
+    statisticHTML() {
+        const div = document.createElement('div');
+        div.className = "statistic";
+
+        const wrapper = document.createElement('div');
+        wrapper.className = "statistic__wrapper";
+
+        const title = document.createElement('h1');
+        title.className = "statistic__title";
+        title.textContent = "Статистика:";
+
+
+
+        const clickCount = document.createElement('div');
+        clickCount.className = 'click-count';
+
+        const clickCountTitle = document.createElement('h2');
+        clickCountTitle.className = 'click-count__title';
+        clickCountTitle.textContent = 'Количество кликов:';
+
+        const clickCountText = document.createElement('p');
+        clickCountText.className = 'click-count__text';
+
+        if(this.localStorage) {
+            clickCountText.textContent = '20';
+        } else {
+            clickCountText.textContent = '0';
+        }
+
+        clickCount.appendChild(clickCountTitle);
+        clickCount.appendChild(clickCountText);
+
+
+        const loseWinCount = document.createElement('div');
+        loseWinCount.className = "lose-win-count";
+
+        const loseWinCountTitle = document.createElement('h2');
+        loseWinCountTitle.className = 'lose-win-count__title';
+        loseWinCountTitle.textContent = 'Победа / Проигрыш';
+
+        const loseWinCountText = document.createElement('p');
+        loseWinCountText.className = 'lose-win-count__text';
+
+        if(this.localStorage) {
+            loseWinCountText.textContent = '20 / 10';
+        } else {
+            loseWinCountText.textContent = '0 / 0';
+        }
+
+        loseWinCount.appendChild(loseWinCountTitle);
+        loseWinCount.appendChild(loseWinCountText);
+
+        wrapper.appendChild(loseWinCount);
+        wrapper.appendChild(clickCount);
+
+        div.appendChild(wrapper);
+
+        return div;
     }
 }
